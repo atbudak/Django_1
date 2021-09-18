b686c194e818a88a4c740e06d531ea9da2c6eb43
ahmetbudak <ahmet1.budak@gmail.com>
2021-09-18 21:51:36 +0300

    Last commit

--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
-from .models import Meetup
+from .models import Meetup, Participants
+from .forms import RegistrationForm
 
 
 def index(request):
@@ -15,13 +16,29 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                user_email = registration_form.cleaned_data['email']
+                participant, _ = Participants.objects.get_or_create(email=user_email)
+                selected_meetup.participants.add(participant)
+                return redirect('confirm-registration', meetup_slug=meetup_slug)
+
         return render(request, 'meetups/meetup_details.html', {
             'meetup_found': True,
-            'meetup_title': selected_meetup.title,
-            'meetup_description': selected_meetup.description
-        })
+            'meetup': selected_meetup,
+            'form': registration_form
+            })
     except Exception as exc:
         return render(request, 'meetups/meetup_details.html', {
             'meetup_found': False
         })
 
+
+def confirm_registration(request, meetup_slug):
+    meetup = Meetup.objects.get(slug=meetup_slug)
+    return render(request, 'meetups/registration-success.html', {
+        'organizer_email': meetup.organizer_email
+    })
